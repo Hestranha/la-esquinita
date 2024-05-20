cb509f29ab774c95ec288b0797913ed022fc15fc
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-05-19 20:46:20 -0500

    funcin nextui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import "./ui/global.css"
+import { Button, ButtonGroup } from "@nextui-org/button";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <Button color="primary">
+        Button
+      </Button>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -27,6 +29,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  darkMode: "class",
+  plugins: [require("@tailwindcss/forms"),nextui()],
 };
 export default config;
